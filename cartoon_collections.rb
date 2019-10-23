6b91f1fed756682850598e7041f40634a91b7056
J <jmrumingan@gmail.com>
2019-10-23 15:29:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,6 +30,8 @@ def find_the_cheese(array)
   
   array.each_with_index do |index|
     #checks each array via index, needs to find matching inside cheese_types 
-    
+    cheese_types.each_with_index do |cheese|
+      
+    end 
   end 
 end
